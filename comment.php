f1f0b7fec793f22943c836ca7bd7791d7fadadba
Md Parvez Ahamed <devparvez23@gmail.com>
2024-04-05 21:15:07 +0600

    single and multi-line comment

---
+++ comment.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Single line Comment 
+    echo "<h1>Welcome Home</h1>";
+
+
+    // Multi-line Comment
+    // echo "<p><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse nam magnam aliquam vitae minima unde sequi! Amet, ea! Voluptates amet ex rerum saepe officiis porro natus eos quis optio veniam magnam consequuntur possimus, placeat repellat alias sed nihil quod quas dolore maxime facilis eaque minima? Aliquid nihil nostrum eligendi sed?</p></p>"
+
+    
+    
+    ?>
+</body>
+</html>
