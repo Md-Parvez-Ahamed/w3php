02c62248506ddd5913b7aa32b3f986258ad4134a
Md Parvez Ahamed <devparvez23@gmail.com>
2024-04-06 18:46:24 +0600

    Php varaiable

---
+++ variable.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Declaring Php Variable</h1>
+    <hr>
+    <?php
+    
+    $text = "PHP";
+
+    echo " My Favorite Programming Language is ".$text;
+
+    $first_Name= "Md.Parvez";
+    $last_Name= "Ahamed";
+
+    echo '<h1>My Name is '.$first_Name.$last_Name .'</h1>';
+    
+    ?>
+</body>
+</html>
